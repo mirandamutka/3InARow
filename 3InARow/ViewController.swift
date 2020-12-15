8eb538e9d9f7d7ea2c54735187b67e32e5382c4f
mirandamutka <michirisu@outlook.com>
2020-12-15 15:32:37 +0100

    Initial commit with base code

---
+++ 3InARow/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  3InARow
+//
+//  Created by Miranda Mutka on 2020-12-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var playerNameLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        playerNameLabel.text = "PlayerName"
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
